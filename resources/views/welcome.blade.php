3ab148713a454f88e22fc1a7e54813fcefbc7d9c
Bharat Tharwani <bharat@ranium.in>
2017-12-10 02:32:32 +0530

    Auth, Vue and web pack ready

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,7 +6,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
         <title>Laravel</title>
-
+        <link rel="stylesheet" type="text/css" href="css/app.css">
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
 
@@ -65,7 +65,7 @@
         </style>
     </head>
     <body>
-        <div class="flex-center position-ref full-height">
+        <div class="flex-center position-ref full-height" id="app">
             @if (Route::has('login'))
                 <div class="top-right links">
                     @auth
@@ -89,7 +89,11 @@
                     <a href="https://forge.laravel.com">Forge</a>
                     <a href="https://github.com/laravel/laravel">GitHub</a>
                 </div>
+                <div>
+                    <example-component></example-component>
+                </div>
             </div>
         </div>
+        <script type="text/javascript" src="js/app.js"></script>
     </body>
 </html>
